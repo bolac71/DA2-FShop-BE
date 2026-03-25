17c3e4a605c9b7e99aeea24c288c43f3e1733fc2
anhlandibo <ngominhtri9107@gmail.com>
2026-03-25 09:05:55 +0700

    feat: implement getMyNotifications endpoint with pagination and sorting

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -1,9 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { CreateNotificationDto } from './dtos';
+import { CreateNotificationDto, QueryNotificationDto } from './dtos';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { ApiBasicAuth, ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get my notifications with pagination and sorting' })
+  getMyNotifications(
+    @Req() req: Request,
+    @Query() query: QueryNotificationDto,
+  ) {
+    const { sub } = req['user'];
+    return this.notificationsService.getMyNotifications(sub, query);
+  }
 }
--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Notification, User } from 'src/entities';
 import { Repository } from 'typeorm';
 import { NotificationGateway } from './notifications.gateway';
-import { CreateNotificationDto } from './dtos';
+import { CreateNotificationDto, QueryNotificationDto } from './dtos';
 
 @Injectable()
 export class NotificationsService {
@@ -32,5 +32,32 @@ export class NotificationsService {
     this.notiGateway.sendToUser(userId, savedNoti);
     
     return savedNoti;
+  }
+
+  async getMyNotifications(userId: number, query: QueryNotificationDto) {
+    const {
+      page,
+      limit,
+      sortBy = 'createdAt',
+      sortOrder = 'DESC',
+      type,
+      isRead
+    } = query;
+
+    const [data, total] = await this.notificationRepository.findAndCount({
+      where: { user: { id: userId }, type, isRead: isRead === undefined ? undefined : isRead === true },
+      ...(page && limit && { take: limit, skip: (page - 1) * limit }),
+      order: { [sortBy]: sortOrder, isRead: 'ASC' },
+    });
+
+    const response = {
+      pagination: {
+        total,
+        page,
+        limit,
+      },
+      data,
+    };
+    return response;
   }
 }
